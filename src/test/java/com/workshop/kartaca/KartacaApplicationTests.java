c347eb2f688918ce44a7c1a4c322dae81708201e
Berkan <berkansahanofficial@gmail.com>
2023-03-24 17:09:45 +0300

    Initial commit

---
+++ src/test/java/com/workshop/kartaca/KartacaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.kartaca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KartacaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
