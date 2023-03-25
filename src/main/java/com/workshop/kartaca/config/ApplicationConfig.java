52f7f8a10ff4a725569743c47a7ee02ee44b9a17
Berkan <berkansahanofficial@gmail.com>
2023-03-25 22:27:19 +0300

    service

---
+++ src/main/java/com/workshop/kartaca/config/ApplicationConfig.java
@@ -0,0 +1,19 @@
+package com.workshop.kartaca.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> null;
+    }
+}
