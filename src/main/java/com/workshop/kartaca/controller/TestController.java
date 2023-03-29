df7af57a6d2da89847a2775bc448ca46286d2c81
Berkan <berkansahanofficial@gmail.com>
2023-03-29 16:42:13 +0300

    Update TestController.java

--- src/main/java/com/workshop/kartaca/controller/TestController.java
+++ src/main/java/com/workshop/kartaca/controller/TestController.java
@@ -1,11 +1,13 @@
 package com.workshop.kartaca.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/test")
 public class TestController {
 
